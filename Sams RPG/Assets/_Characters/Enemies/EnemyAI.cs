1b4c29330cf52c753eaf1066d2bdcc890d17e2a2
DESKTOP-PBH6C6B\SamDower <samdower@hotmail.co.uk>
2018-01-16 20:52:14 +0000

    Enum Structure For EnemyAI

--- Sams RPG/Assets/_Characters/Enemies/EnemyAI.cs
+++ Sams RPG/Assets/_Characters/Enemies/EnemyAI.cs
@@ -8,19 +8,46 @@ namespace RPG.Characters {
 
 	    [SerializeField] float chaseRadius = 6f;
 
-	    bool isAttacking = false;
-		PlayerMovement player = null;
+		enum State { idle, patrolling, attacking, chasing }
+		State state = State.idle;
+
+		PlayerMovement player;
+		Character character;
+
 		float currentWeaponRange;
+		float distanceToPlayer;
 
 	    void Start() {
 			player = FindObjectOfType<PlayerMovement> ();
-	    }
+			character = GetComponent<Character> ();
+		}
 
 	    void Update() {
-	        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+	        distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
 			WeaponSystem weaponSystem = GetComponent<WeaponSystem> ();
 			currentWeaponRange = weaponSystem.GetCurrentWeapon ().GetMaxAttackRange ();
+
+			if (distanceToPlayer > chaseRadius && state != State.patrolling) {
+				StopAllCoroutines ();
+				state = State.patrolling; // TODO Replace to use coroutine
+			}
+			if (distanceToPlayer <= chaseRadius && state != State.chasing) {
+				StopAllCoroutines ();
+				StartCoroutine (ChasePlayer ());
+			}
+			if (distanceToPlayer <= currentWeaponRange && state != State.attacking) {
+				StopAllCoroutines ();
+				state = State.attacking; // TODO Replace to use coroutine
+			}
 	    }
+
+		IEnumerator ChasePlayer() {
+			state = State.chasing;
+			while (distanceToPlayer >= currentWeaponRange) {
+				character.SetDestination (player.transform.position);
+				yield return new WaitForEndOfFrame ();
+			}
+		}
 
 	    void OnDrawGizmos() {
 	        // Draw attack sphere 
